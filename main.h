12def5d957fa0d12d809899216d0beb276caded7
wambuastephen <wambuastephen97@gmail.com>
2023-10-09 12:14:21 +0000

    task1

--- main.h
+++ main.h
@@ -1,12 +1,16 @@
-#ifndef _MAIN_
-#define _MAIN_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+#define MAX_INPUT_LENGTH 1024
+#define MAX_ARGS 64
 
-
-
-
+/* Function declaration for wordparser_args */
+int wordsparser_args(char *input, char *args[]);
 #endif
